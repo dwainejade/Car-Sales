f2cb2bcf3b882320a35dea37c17b4246e9c6f5ba
dwainejade <dwainejade@gmail.com>
2020-08-23 13:13:13 -0400

    restarted

--- src/components/AdditionalFeature.js
+++ src/components/AdditionalFeature.js
@@ -1,10 +1,14 @@
 import React from 'react';
+import { addFeature } from "../actions/carActions";
 
 const AdditionalFeature = props => {
   return (
     <li>
       {/* Add an onClick that will let you add a feature to your car */}
-      <button className="button">Add</button>
+      <button className="button"
+      disabled={props.feature.disabled}
+      onClick={() => props.addFeature(props.feature)}
+      >Add</button>
       {props.feature.name} (+{props.feature.price})
     </li>
   );
